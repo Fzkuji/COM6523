b7c0a63089a241bb8b2d32e2899ce71e8c07ac5e
Fu Zichuan <zfu13@sheffield.ac.uk>
2022-04-11 15:05:49 +0100

    Upload part 1 report, part 3 experiment.

--- submission/initial-analysis/initial-analysis.sh
+++ submission/initial-analysis/initial-analysis.sh
@@ -1,6 +1,8 @@
 chmod +x listFiles.sh
 chmod +x listFileSizes.sh
 chmod +x repMining.sh
+chmod +x targetMining.sh
 ./listFiles.sh joda-time/src/main '*.java' > javaFiles.csv
 ./listFileSizes.sh joda-time/src/main '*.java' > javaFileSizes.csv
 ./repMining.sh joda-time > repCommits.csv
+./targetMining.sh joda-time "src/main/java/org/joda/time/*.java" > javaFileCommits.csv
